33ccf19d0b7e8633a1fa1256b9be69b74ce275b0
Jayesh Bagde <jysh.bgde2001@gmail.com>
2023-05-26 12:43:23 +0530

    updated mongoose connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     try {
-        const conn = mongoose.connect(process.env.MONGO_URI);
+        mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
         
     } catch (error) {
         console.error(`Error : ${error.message}`);
