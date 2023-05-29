5bcafa9ab251983cdc529c1238e5dc29117ec8c4
Jayesh Bagde <jysh.bgde2001@gmail.com>
2023-05-29 12:46:18 +0530

    Updated search box with dropdown

--- client/src/screens/MovieScreen/MovieScreen.jsx
+++ client/src/screens/MovieScreen/MovieScreen.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/Header'
 import { Container, Row , Col, Form, Button} from 'react-bootstrap'
 import '../MovieScreen/MovieScreen.css'
 import Cards from '../../components/Cards'
 import { toast } from 'react-toastify'
 import { client } from "@gradio/client";
+import movieTitles from './utils'
+
 
 
 const MovieScreen = () => {
@@ -12,7 +14,13 @@ const MovieScreen = () => {
   const endpoint = "https://jyshbgde-cinescope.hf.space";
   
   const [movie, setMovie] = useState("")
+
   const [recommendations, setReccomendations] = useState("")
+  
+  const [movieTitlesList, setMovieTitlesList] = useState(movieTitles)
+  
+
+ 
   const submitHandler = async (e) => {
     
     e.preventDefault()
@@ -41,19 +49,41 @@ const MovieScreen = () => {
         <Row>
             <Col>
             <Form className="d-flex" onSubmit={submitHandler}>
-            <Form.Control
-              type="search"
+            <input type='search'
+            list='movies'
+              name='movies'
               placeholder="Enter movie name"
-              className="me-2"
+              className="me-2 movieInput"
               aria-label="Search"
               value={movie}
               onChange={(e)=>setMovie(e.target.value)}
-            />
+              autoComplete='on'
+            /> 
+
+             <datalist id="movies" className='movieTitlesList'>
+              
+          {
+           movieTitlesList.map((movie,index) => {
+            return (
+              <option value={movie} key={index} />
+            )
+           })
+          }
+          
+</datalist>
+            
+
+            
+            
+           
+           
             <Button type='submit' variant="outline-light">Search</Button>
           </Form>
             </Col>
         </Row>
     </Container>
+    
+
     <Container fluid className='cardContainer'>
 
     <Cards recommendations = {recommendations}/>
